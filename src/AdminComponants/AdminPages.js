491a3517b09db47031db318aae854ab1c38403ac
acher <acherklein0@gmaicom>
2020-09-29 12:55:27 +0300

    test admin Security

--- src/AdminComponants/AdminPages.js
+++ src/AdminComponants/AdminPages.js
@@ -1,6 +1,8 @@
-import {Button, Form, FormControl, InputGroup} from "react-bootstrap";
+import {Button, Form, FormControl, InputGroup,Card} from "react-bootstrap";
 import  {Link} from "react-router-dom";
 import {Redirect} from "react-router-dom";
+import {trash} from "react-bootstrap-icons"
+
 import React from "react";
 import Axios from "axios";
 
@@ -8,19 +10,55 @@ import Axios from "axios";
 export default class AdminPages extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {redirect:false}
+        this.state = {redirect:false,listOfpersonal:null,loading:true}
+        this.userRecup()
     }
 
     async userRecup()
     {
-        var test =await  Axios.get(this.props.URL + "/recupAllUser",{withCredentials:true})
-        console.log(test)
+        var objetPersonal =await  Axios.get(this.props.URL + "/recupAllUser",{withCredentials:true})
+        if(objetPersonal.data == null)
+        {
+            this.setState({listOfpersonal:null,loading:true})
+            setTimeout(this.userRecup(),2000)
+        }
+        else
+        {
+            this.setState({listOfpersonal:objetPersonal.data,loading:false})
+        }
+
     }
 
+    async afficheData(iod)
+    {
+
+    }
+    async makeItAdmin(id)
+    {
+
+    }
     render()
     {
-        this.userRecup()
-        return ""
+        return this.state.loading ? <><p>loading...</p></> :
+            <>
+                {this.state.listOfpersonal.map((value,index)=>
+                {
+                    return <Card style={{ width: '18rem',display:"inline-block"}}>
+                        <Card.Body>
+                            <Card.Title>{value.nom}</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                            <Card.Text>
+                                {value.prenom}
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <Button type="primary" onClick={this.afficheData(value._id)} />
+                            <Button type="primary" onClick={this.makeItAdmin(value._id)} />
+                            <Button><thrash/></Button>
+                        </Card.Footer>
+                    </Card>
+                })}
+            </>
     }
 }
 
