9076720d3587ced78f96be5041498d24b6abfd66
acher <acherklein0@gmaicom>
2020-09-30 13:13:13 +0300

    google maps

--- src/GlobalPages/GoogleMaps.js
+++ src/GlobalPages/GoogleMaps.js
@@ -1,13 +1,16 @@
+import Slider from '@material-ui/core/Slider';
+import {Col,Row} from "react-bootstrap";
 import React from "react";
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import {Map, InfoWindow, Marker,Circle, GoogleApiWrapper} from 'google-maps-react';
 import Axios from "axios";
+
 
 
 class GoogleMaps extends React.Component
 {
     constructor(props) {
         super(props);
-        this.state = {Loading:true,latitude:0,longitude:0,categorie:null}
+        this.state = {Loading:true,latitude:0,longitude:0,categorie:null,radius:600}
         //this.recupCategorie();
         this.recupCoord()
     }
@@ -29,17 +32,43 @@ class GoogleMaps extends React.Component
     render()
     {
         return this.state.Loading ? <p>...Load</p> :
-            <Map google={this.props.google}
-            initialCenter={{
-            lat: this.state.latitude,
-            lng: this.state.longitude
-        }}
-        style={{width:"98vw",height:"90vh"}}
-        >
-        <Marker>
+            <><Row> <Col md={3} style={{backgroundColor:"black",height:"90vh"}}>
+                <h1 style={{color:"white"}}>SlideRange :</h1>
+                <Slider
+                    defaultValue={600}
+                    aria-labelledby="discrete-slider-small-steps"
+                    step={100}
+                    onChange={(e,value)=>{this.setState({radius:value})}}
+                    min={0}
+                    max={10000}
+                    valueLabelDisplay="auto"
+                />
+                <div>ObjectChose</div>
+            </Col>
+                <Col>
+                    <Map google={this.props.google}
+                        initialCenter={{
+                        lat: this.state.latitude,
+                        lng: this.state.longitude
+                        }}
+                        style={{width:"98vw",height:"90vh"}}
+                    >
 
-        </Marker>
-        </Map>
+
+                        <Circle
+                        radius={this.state.radius}
+                        center={
+                            {lat: this.state.latitude,
+                            lng: this.state.longitude}}
+                        strokeColor='transparent'
+                        strokeOpacity={0}
+                        strokeWeight={5}
+                        fillColor='#FF0000'
+                        fillOpacity={0.2}/>
+                    </Map>
+           </Col>
+            </Row>
+            </>
     }
 
 }
