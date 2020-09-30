d32a0f1994bdb17e9fc0c606c98eba6a40aacaa0
Yaacov9 <elbaz.yaacov@gmail.com>
2020-09-30 22:38:19 +0300

    Supprimer des objet depuis manage items

--- src/ItemsForms/ManageItems.js
+++ src/ItemsForms/ManageItems.js
@@ -8,71 +8,63 @@ import Axios from "axios";
 export default class ManageItems extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {loading:false,objList:null,obj:null,show:false}
+        this.state = {loading: false,objList:null, obj: null, show: false}
         this.recupAllMyItems()
     }
 
-    async onHide()
-    {
-        this.setState({show:false})
+    async onHide() {
+        this.setState({show: false})
     }
-    async recupAllMyItems()
-    {
+
+    async recupAllMyItems() {
         var proprioID = this.props.user._id;
-        var allMyItems = await  Axios.post(this.props.URL + "/getAllObjfromUser",{proprioID},{withCredentials:true})
-        if(allMyItems.data == null)
-        {
-            this.setState({objList:null})
-        }
-        else
-        {
-            this.setState({objList:allMyItems.data,loading:false})
+        var allMyItems = await Axios.post(this.props.URL + "/getAllObjfromUser", {proprioID}, {withCredentials: true})
+        if (allMyItems.data == null) {
+            this.setState({objList: null})
+        } else {
+            await this.setState({objList: allMyItems.data, loading: false})
         }
     }
-    async getItemById(id)
-    {
-        var item = await Axios.post(this.props.URL + "/getItemById",{id:id},{withCredentials:true})
-        this.setState({obj:item.data,show:true})
-    }
-    async remove(id)
-    {
-        var response = await Axios.post(this.props.URL + "/removeUser",{id:id},{withCredentials:true})
-        this.userRecup()
-    }
-    render()
-    {
-        return this.state.loading ? <><p>loading...</p></> :
-            <>
 
-                        <Card style={{ width: '18rem',display:"inline-block"}}>
+    async getItemById(id) {
+        var item = await Axios.post(this.props.URL + "/getItemById", {id: id}, {withCredentials: true})
+        this.setState({obj: item.data, show: true})
+    }
+
+    async removeObjById(id) {
+        console.log(id,"FEGFNHJ");
+        await Axios.post(this.props.URL + "/removeObjById", {id: id}, {withCredentials: true})
+        await this.recupAllMyItems();
+    }
+
+    render() {
+        {
+            return this.state.loading ? <><p>loading...</p></> :
+                <>
+                    {this.state.objList?.map((value, index) => {
+                        return <Card style={{width: '18rem', display: "inline-block"}}>
                             <Card.Body>
-                                <Card.Title></Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
-                            </Card.Body>
-                            <Card.Footer>
-                            </Card.Footer>
-                        </Card>
-                        <Card style={{ width: '18rem',display:"inline-block",color:"red"}}>
-                            <Card.Body>
-                                <Card.Title></Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+                                <Card.Title>{value.name}</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">{value.nom}</Card.Subtitle>
                                 <Card.Text>
-
+                                    {value.prenom}
                                 </Card.Text>
                             </Card.Body>
                             <Card.Footer>
-                                <Button type="primary" onClick={()=>this.afficheData} > affichage</Button>
-                                <Button onClick={()=>this.remove()}><Trash/></Button>
+                                <Button type="primary" onClick={() => this.afficheData(value._id)}> affichage</Button>
+                                <Button onClick={() => this.removeObjById(value._id)}><Trash/></Button>
                             </Card.Footer>
                         </Card>
-                <Modal show={this.state.show} onHide={this.onHide.bind(this)}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>{this.state.user?.nom}</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <UserProfil URL={this.props.URL} user={this.state.user} desactivate={true}/>
-                    </Modal.Body>
-                </Modal>
-            </>
+                    })}
+                    <Modal show={this.state.show} onHide={this.onHide.bind(this)}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>{this.state.user?.nom}</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <UserProfil URL={this.props.URL} user={this.state.user} desactivate={true}/>
+                        </Modal.Body>
+                    </Modal>
+                </>
+        }
     }
 }
